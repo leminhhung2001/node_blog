7fb9b86e5612b1dd035da8a203c72f80422fddaa
leminhhung2001 <leminhhungtabletennis@gmail.com>
2021-02-28 17:00:55 +0700

    Bootstrap 4

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars  = require('express-handlebars')
+
+const app = express()
+
+
+const port = 3000
+
+//Template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+// console.log(__dirname)
+// console.log('PATH: ', path.join(__dirname, 'resources/views'))
+
+//Lay anh ra bang static
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Http logger
+app.use(morgan('combined'))
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
